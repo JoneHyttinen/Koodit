9c7eff7b55fdffed383d88c54049760ae2ca474f
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2025-03-02 11:47:00 +0200

    commit

---
+++ Learn JavaScript-course/index.js
@@ -0,0 +1,28 @@
+// document.getElementById("count-el").innerText = 5
+
+//let count = 5
+//console.log(count)
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function numero() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+let lapsCompleted = 0
+
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted)
