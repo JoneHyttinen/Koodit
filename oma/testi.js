db4bf3afe3e72905a4ec1c917f3fec25cab26282
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2025-04-25 16:12:56 +0300

    commit

--- oma/testi.js
+++ oma/testi.js
@@ -1,20 +1,28 @@
-let opiskelija = {
-  tunnus: "a1234567",
-  nimi: {
-    etunimi: "Pekka",
-    sukunimi: "Virtanen",
+let suoritukset = [
+  {
+    nimi: "Johdanto digitaalisiin palveluihin",
+    tunnus: "DIG001IT1A",
+    arvosanat: [
+      { paiva: "14.03.2023", arvosana: "3" },
+      { paiva: "21.03.2023", arvosana: "2" },
+      { paiva: "02.04.2023", arvosana: "4" },
+    ],
   },
-  osoite: {
-    lahiosoite: "Helsinginkatu 11",
-    postinumero: "00100",
-    postitoimipaikka: "Helsinki",
+  {
+    nimi: "Johdanto liiketoimintalähtöiseen ICT:hen",
+    tunnus: "ICB001IT1A",
+    arvosanat: [{ paiva: "15.03.2023", arvosana: "4" }],
   },
-  puhelimet: [
-    { numero: "050-123456", tyyppi: "työ" },
-    { numero: "050-7654321", tyyppi: "henkilökohtainen" },
-    { numero: "050-1234321", tyyppi: "salainen" },
-  ],
-};
-let numeroTyyppi = opiskelija.puhelimet[0].tyyppi;
+  {
+    nimi: "Johdanto ICT-infrastruktuuriin ja pilvipalveluihin",
+    tunnus: "ICI001IT1A",
+    arvosanat: [
+      { paiva: "16.03.2023", arvosana: "1" },
+      { paiva: "23.03.2023", arvosana: "4" },
+    ],
+  },
+];
 
-console.log(numeroTyyppi);
+let kurssiTunnus = suoritukset[0].tunnus;
+let suorituksenPaiva = suoritukset[0].arvosanat[0].paiva;
+console.log(suorituksenPaiva);
